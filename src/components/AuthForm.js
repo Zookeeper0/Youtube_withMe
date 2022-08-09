727abaf8ce868339c9d706578bbfb9adf14bba54
eastZoo <dongju010@naver.com>
2022-08-09 23:00:52 +0900

    feat : login recommand  text

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -9,44 +9,44 @@ const AuthForm = () => {
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
     const onChange = (event) => {
-        const {target: {name, value},
+        const { target: { name, value },
         } = event;
         if (name == "email") {
             setEamil(value);
-        } else if ( name === "password" ) {
-            setPassword(value); 
+        } else if (name === "password") {
+            setPassword(value);
         }
     };
 
-    const onSubmit = async(event) => {
+    const onSubmit = async (event) => {
         // submit 했을때 기본적으로 일어나는 페이지 새로고침 방지
         event.preventDefault();
         try {
             let data;
             const auth = getAuth();
-            if(newAccount){
+            if (newAccount) {
                 //create account
                 await createUserWithEmailAndPassword(
-                    auth ,email, password
-                    );
+                    auth, email, password
+                );
             } else {
                 // log in
                 await signInWithEmailAndPassword(
                     auth, email, password
-                    );
+                );
             }
             console.log(data);
         } catch (error) {
             setError(error.message);
         }
     };
-    return(
+    return (
         <>
             <form onSubmit={onSubmit} className="container">
                 <input
                     name="email"
-                    type="email" 
-                    placeholder="Email" 
+                    type="email"
+                    placeholder="Email"
                     required value={email}
                     onChange={onChange}
                     className="authInput"
@@ -54,14 +54,14 @@ const AuthForm = () => {
                 <input
                     name="password"
                     type="password"
-                    placeholder="Password" 
-                    required 
+                    placeholder="Password"
+                    required
                     value={password}
                     onChange={onChange}
                     className="authInput"
                 />
-                <input 
-                    type="submit" 
+                <input
+                    type="submit"
                     className="authInput authSubmit"
                     value={newAccount ? "회원가입" : "로그인"}
                 />
@@ -70,8 +70,9 @@ const AuthForm = () => {
             <span onClick={toggleAccount} className="authSwitch">
                 {newAccount ? "로그인" : "회원가입"}
             </span>
+            <div style={{ marginBottom: 10 }}>👉🏻 구글이나 깃허브로 로그인 추천😎</div>
         </>
-        
+
     );
 };
 
