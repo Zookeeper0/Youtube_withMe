004e6c9c90b8378cb0349e348957f3beb88a6cd5
eastZoo <dongju010@naver.com>
2022-08-10 13:19:05 +0900

    fix : CORS errer #4

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/proxy',
+        '/',
         createProxyMiddleware({
             target: 'https://rlp-proxy.herokuapp.com',
             changeOrigin: true,
